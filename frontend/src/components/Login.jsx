0cec834057a2bab51ed1b0b1f85d23b47e71fada
Yuichi <yuichi@pop-os.localdomain>
2023-08-18 15:00:14 +0530

    Solved Login Error

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { GoogleLogin } from '@react-oauth/google';
+import React, { useEffect } from 'react';
+import { GoogleLogin } from "react-google-login";
+import { gapi } from 'gapi-script';
 import { useNavigate } from 'react-router-dom';
-import { FcGoogle } from 'react-icons/fc';
 import pixelVideos from '../assets/pixel.mp4';
 import logo from '../assets/logo.png';
 const Login = () => {
-  const responseGoogle = (response) => {
-    console.log(response)
-  }
+
+  useEffect(() => {
+    gapi.load('client:auth2', () => {
+      gapi.auth2.init({ clientId: process.env.REACT_APP_GOOGLE_API_TOKEN })
+    })
+  }, [])
+
+
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
       <div className='relative w-full h-full'>
@@ -21,20 +26,20 @@ const Login = () => {
         />
         <div className='flex flex-col absolute justify-center items-center top-0 bottom-0 right-0 left-0 bg-blackOverlay'>
           <div className='p-5'>
-            <img src={logo} alt="logo" className='w-45 h-40 rounded-full' />
+            <img src={logo} alt="logo" className='w-45 h-40 rounded-2xl' />
           </div>
           <div className='flex rounded-lg'>
-            
 
             <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              buttonText="Sign In with Google"
               onSuccess={credentialResponse => {
                 console.log(credentialResponse);
               }}
-              onError={() => {
-                console.log('Login Failed');
-              }}
-              Cross-Origin-Opener-Policy='same-origin-allow-popups'
-            />;
+              isSignedIn={true}
+              cookiePolicy={"single_host_origin"}
+
+            />
 
 
           </div>
